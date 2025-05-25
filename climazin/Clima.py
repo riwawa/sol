8cac3ff88817f51e7730bfb1898e044a169e4c54
bia <beatrizlucena2007@gmail.com>
2025-05-25 21:09:02 +0000

    merge

--- climazin/Clima.py
+++ climazin/Clima.py
@@ -1,25 +1,21 @@
 from shiny import App, ui, render, reactive
 import pandas as pd
-import numpy as np
 from cidades import cidades
-from ClimaAPI import buscar_dados_clima, obter_coordenadas, grafico_temperatura, gerar_mapa_temperatura, grafico_chuva
+from ClimaAPI import (
+    buscar_dados_clima,
+    grafico_temperatura,
+    gerar_mapa_temperatura,
+    grafico_chuva
+)
 import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
-import matplotlib.cm as cm
-import matplotlib.colors as mcolors
-
-
-print("Carregando dados...")
 
 anos = list(range(2000, 2025))
 
+# === UI ===
 app_ui = ui.page_sidebar(
     ui.sidebar(
         ui.input_select("tipo_dado", "Tipo de dado:", choices=["Temperatura", "Precipitação", "Vento"]),
         ui.input_select("tipo_vis", "Tipo de visualização:", choices=["Gráfico", "Mapa"]),
-
-        # ui.input_select("modo", "Modo de visualização:", choices=["Gráfico", "Mapa", "Chuva"], selected="Gráfico"),
         ui.input_select('cidade', 'Escolha a cidade:', choices=cidades),
         ui.input_slider('ano', 'Escolha o ano:', min=min(anos), max=max(anos), value=min(anos), step=1),
         style='background-color: white; height: 100vh;'
@@ -28,25 +24,31 @@ app_ui = ui.page_sidebar(
         12,
         ui.h2('Visualizador Climático'),
         ui.div(
+            ui.h2(
+                'Visualizador Climático', style='background-color: #004578; color: white; font-family: "Arial Black"; padding: 10px 20px; border-radius: 10px;'
+            ),
+        ),
+        ui.div( # Gráfico
+            ui.panel_conditional("input.tipo_dado == 'Temperatura' && input.tipo_vis == 'Gráfico'", ui.output_plot('graficoTemp')),
+            ui.panel_conditional("input.tipo_dado == 'Temperatura' && input.tipo_vis == 'Mapa'", ui.output_plot('mapaTemp')),
+            ui.panel_conditional("input.tipo_dado == 'Precipitação' && input.tipo_vis == 'Gráfico'", ui.output_plot('graficoChuva')),
+            ui.panel_conditional("input.tipo_dado == 'Precipitação' && input.tipo_vis == 'Mapa'", ui.output_ui("avisoMapa")),
+            ui.panel_conditional("input.tipo_dado == 'Vento' && input.tipo_vis == 'Mapa'", ui.output_plot('ventoMapa')),
+            ui.panel_conditional("input.tipo_dado == 'Vento' && input.tipo_vis == 'Gráfico'", ui.output_ui("avisoGrafico")),
 
-            style='background: linear-gradient(to bottom, #87cefa, white); padding: 20px; border-radius: 10px; width: 100%; box-shadow: 0 4px 20px rgba(0, 0, 0 , 0.3);'           
-        ),    
+            style='background: linear-gradient(to bottom, #004578, #7ba8c9); padding: 10px; border-radius: 10px; width: 100%; box-shadow: 0 4px 20px rgba(0, 0, 0 , 0.3); margin-top: 50px;'
+        ),
         ui.tags.style("""
             body {
-            background: linear-gradient(to bottom, #a6a6a6, white);
-            min-height: 100vh;
-            margin: 0;
-        }
-     """),
-        
-        
-    ),
-
-
+                background: linear-gradient(to bottom, #87cefa, white);
+                min-height: 100vh;
+                margin: 0;
+            }
+        """),
+    )
 )
 
 def server(input, output, session):
-
     @output
     @render.ui
     def avisoMapa():
@@ -68,14 +70,6 @@ def server(input, output, session):
     def graficoTemp():
         df = dados_filtrados()
         return grafico_temperatura(df, input.cidade(), input.ano())
-        
-    @output
-    @render.plot
-    def mapaTop():
-        cidade = input.cidade()
-        ano = input.ano()
-        fig = gerar_mapa_temperatura(cidade, ano)
-        return fig
 
     @output
     @render.plot
@@ -85,7 +79,16 @@ def server(input, output, session):
 
     @output
     @render.plot
-    def mapaVento():
+    def graficoVento():
+ 
+
+    @output
+    @render.plot
+    def mapaTop():
+        cidade = input.cidade()
+        ano = input.ano()
+        fig = gerar_mapa_temperatura(cidade, ano)
+        return fig
 
 app = App(app_ui, server)
 
